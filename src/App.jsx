1c06070d67c5b32c0d30fa2635ee746ba5074936
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-06-11 17:42:52 -0400

    Start.

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+/** Component for entire page.
+ *
+ * Props: none
+ * State: none
+ *
+*/
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function incrCount() {
+    setCount(count => count + 1);
+  }
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+};
+
+export default App;

e5d26875eb30ab1f23b8e302499237282af32070
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-06-12 17:56:38 -0400

    Added a form for logging in

---
+++ src/LoginForm.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+const INITIAL_FORM_DATA = {
+  username: "",
+  password: "",
+};
+
+/** Handles logging in a user.
+ *
+ * Props:
+ *
+ * State:
+ *
+ * RouteList -> LoginForm
+ */
+function LoginForm({ logIn }) {
+
+  const [formData, setFormData] = useState(INITIAL_FORM_DATA);
+
+  function handleChange(evt) {
+    const { name, value } = evt.target;
+    setFormData(currFormData => ({ ...currFormData, [name]: value }));
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    logIn(formData);
+  }
+
+  return (
+    <form className="LoginForm" onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="LoginForm-username" className="col-5">Username</label>
+        <input
+          id="LoginForm-username"
+          className="col7"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="LoginForm-password" className="col-5">Password</label>
+        <input
+          id="LoginForm-password"
+          className="col7"
+          name="password"
+          type="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </div>
+
+      <button>Log In</button>
+    </form>
+  );
+}
+
+export default LoginForm;

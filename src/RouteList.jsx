96c11d383abd77ea71f78254c98534a8199b9f9e
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-06-12 17:56:51 -0400

    Renamed; added guard for routes

---
+++ src/RouteList.jsx
@@ -0,0 +1,39 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+import Home from "./Home.jsx";
+import LoginForm from "./LoginForm.jsx";
+import SignupForm from "./SignupForm.jsx";
+
+/**
+ * Purpose: route to different components based on URLs
+ *
+ * Props:
+ *  - currentUser
+ *  - uploadImage
+ *  - signUp
+ *  - logIn
+ *
+ * States: none
+ *
+ *  App > RouteList -> {FriendFinder, Home, SignupForm, LoginForm}
+ */
+function RouteList({ currUser, uploadImage, signUp, logIn }) {
+  return (
+    <div className="RoutesList">
+      {currUser &&
+        <Routes>
+          <Route path="/" element={<FriendFinder />} />
+        </Routes>
+      }
+      {!currUser &&
+        <Routes>
+          <Route path="/" element={<Home uploadImage={uploadImage} />} />
+          <Route path="/signup" element={<SignupForm signUp={signUp} />} />
+          <Route path="/login" element={<LoginForm logIn={logIn} />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      }
+    </div>
+  );
+}
+
+export default RouteList;

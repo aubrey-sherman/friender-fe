989774bfa374d85d28b3ef7d08baa429f7e0a261
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-06-12 17:59:59 -0400

    New component

---
+++ src/FriendFinder.jsx
@@ -0,0 +1,13 @@
+/** Shows possible friends for matching.
+ *
+ * Props:
+ *
+ * State:
+ *
+ * RouteList -> FriendFinder
+ */
+function FriendFinder() {
+  return <p>Find your next friend!</p>;
+}
+
+export default FriendFinder;

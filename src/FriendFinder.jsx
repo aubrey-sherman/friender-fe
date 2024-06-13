dcf164c5ad42b0073f8dfd7635abed39a3417048
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-06-13 15:16:37 -0400

    Side effect

--- src/FriendFinder.jsx
+++ src/FriendFinder.jsx
@@ -1,4 +1,6 @@
 import FriendCard from "./FriendCard.jsx";
+import { useState, useEffect } from "react";
+import FrienderApi from "./api.js";
 
 /** Shows possible friends for matching.
  *
@@ -6,15 +8,36 @@ import FriendCard from "./FriendCard.jsx";
  *  - currUser
  *
  * State:
+ *  - potentialFriends
  *
  * RouteList -> FriendFinder -> FriendCard
  */
 function FriendFinder({ currUser }) {
+  const [potentialFriends, setPotentialFriends] = useState(null);
+
+  useEffect(
+    function loadPotentialFriendsOnMount() {
+
+      async function getPotentialFriends() {
+        const potentialFriends = await FrienderApi.getOtherUsers();
+        setPotentialFriends(potentialFriends);
+      }
+
+      getPotentialFriends();
+    },
+    []
+  );
+
+  // do logic
+
+  if (!potentialFriends) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <div className="FriendFinder">
       <p>Let's find your next friend, {currUser.username}!</p>
-      <FriendCard />
+      <FriendCard friendDetails={potentialFriends[0]} />
     </div>
   );
 }
